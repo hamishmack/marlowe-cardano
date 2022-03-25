cf9faefa3219c126eb716415d6efc256a29c36ae
Shea Levy <shea@shealevy.com>
2022-03-25 13:30:13 -0400

    Fix chain-index flakeref

--- ci/cicero-actions/testnet-node-deploy.nix
+++ ci/cicero-actions/testnet-node-deploy.nix
@@ -127,7 +127,7 @@
               };
 
               config = {
-                packages = [ "${flakeBase}#chainIndex" ];
+                packages = [ "${flakeBase}#chain-index" ];
 
                 command = [ "/bin/entrypoint" ];
               };
